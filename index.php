19ba38f8e4387bd4efa4e8e7829704aeebb83898
kinomakino <kinomakino@hotmail.com>
2024-05-02 20:49:28 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <?php
+        // Esto es un comentario en PHP
+        echo "<h1>Hola Mundo</h1>";
+    ?>
+</body>
+</html>
