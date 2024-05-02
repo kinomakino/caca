9a0fd1104e8ba2ad6f29f614454702c25cacecf2
kinomakino <kinomakino@hotmail.com>
2024-05-02 20:52:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,25 @@
 <html>
 <head>
     <title>Hola Mundo</title>
+    <!-- Add the stylesheet to the head -->
+    <link rel="stylesheet" type="text/css" href="clippy.css" media="all">
 </head>
 <body>
     <?php
         // Esto es un comentario en PHP
         echo "<h1>Hola Mundo</h1>";
     ?>
+
+    <!-- jQuery 1.7+ -->
+    <script src="jquery.1.7.min.js"></script>
+    <!-- Clippy.js -->
+    <script src="clippy.min.js"></script>
+    <!-- Init script -->
+    <script type="text/javascript">
+        clippy.load('Merlin', function(agent){
+            // do anything with the loaded agent
+            agent.show();
+        });
+    </script>
 </body>
 </html>
